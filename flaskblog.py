a58b37df14bb0285bda33e999b68c0ad2f95b0e7
Lucas Campelo <lucas_campelo3009@hotmail.com>
2020-12-28 15:08:37 -0300

    Template rendering function

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='Home')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 app.run(port=5000)
