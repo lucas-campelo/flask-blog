d63440d56adc4d2d03dfd80f6b11691799e48723
Lucas Campelo <lucas_campelo3009@hotmail.com>
2021-01-06 17:39:49 -0300

    Changes in app root

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8bc2a72f05f2962d50de8b63a6f3c158'
 
 posts = [
     {
@@ -28,6 +31,25 @@ def account():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Welcome {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == '1234':
+            flash('Welcome to your account', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Oops, check the fields', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
