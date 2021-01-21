6e25dd1973d1c3b525c723a7de01a13a396928f4
Lucas Campelo <lucas_campelo3009@hotmail.com>
2021-01-21 11:41:36 -0300

    Added user authentication system

--- app/flaskblog/__init__.py
+++ app/flaskblog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8bc2a72f05f2962d50de8b63a6f3c158'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  #  Same as url_for
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
